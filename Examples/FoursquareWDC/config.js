68b46dc114cb9996372a2a2c07d82c9c15c78df1
JeffGlobant <89108925+JeffGlobant@users.noreply.github.com>
2021-08-20 10:06:44 -0500

    Update config.js

--- Examples/FoursquareWDC/config.js
+++ Examples/FoursquareWDC/config.js
@@ -1,1 +1,10 @@
-
+// The necessary configuration for your server
+// Contains credentials for your Foursquare application
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'http://localhost',
+ 'PORT': 3333,
+ 'CLIENT_ID': 'UK41N0NNTULYOSPCA3VJEHJLENWB5ZXMNHFA2CWRSTH1SDNQ',
+ 'CLIENT_SECRET': 'QZJIGQTFEQ3WFKZNO2FZTNEQL4AZRTCTPS002P2SAHHGT0PA',
+ 'REDIRECT_PATH': '/redirect'
+};
